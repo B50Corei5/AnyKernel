b9c0e9d2a758a650cf107331fb028569cc47addc
amoghamaiya <likeavengers@gmail.com>
2018-05-31 12:07:48 +0530

    Update for M1

--- anykernel.sh
+++ anykernel.sh
@@ -4,36 +4,32 @@
 ## AnyKernel setup
 # begin properties
 properties() {
-kernel.string=The BFR™ by amog787
-do.devicecheck=0
-do.initd=1
+kernel.string=Kernel by amog787
+do.devicecheck=1
+do.modules=0
 do.cleanup=1
-do.stuff=0
-do.blobs=0
-device.name1=z2_plus
-device.name2=Z2131
-device.name3=Z2
-device.name4=z2
-device.name5=Z2132
+do.cleanuponabort=0
+do.system_blobs=0
+device.name1=ASUS_X00TD
+device.name2=X00TD
+device.name3=
+device.name4=
+device.name5=
 } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
-
+ramdisk_compression=auto;
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . /tmp/anykernel/tools/ak2-core.sh;
 
-
-## AnyKernel permissions
-# set permissions for included ramdisk files
-chmod -R 755 $ramdisk
-
 ## AnyKernel install
 dump_boot;
-insert_line init.rc "init.cpufreq.rc" after "import /init.usb.rc" "import /init.cpufreq.rc";
+
 write_boot;
 
 ## end install
+
